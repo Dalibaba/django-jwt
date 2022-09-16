0360a5fbc94426d9582953f2499bc4d9787ab9a9
dlinke <david.linke@ibykus.de>
2022-09-16 08:41:16 +0200

    add user model and overwrite django auth user model

--- jwt/users/models.py
+++ jwt/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None # overwrite username field of abstract user
+
+    USERNAME_FIELD = 'email' # login with email instead of password
+    REQUIRED_FIELDS =  []
+

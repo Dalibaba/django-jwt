9050e1a2ee5fe7901c24c8e94d8d3fb5bffe27e2
dlinke <david.linke@ibykus.de>
2022-09-16 13:50:18 +0200

    add password hashing

--- jwt/users/serializers.py
+++ jwt/users/serializers.py
@@ -1,4 +1,5 @@
 from pyexpat import model
+from wsgiref import validate
 from rest_framework import serializers
 from .models import User
 
@@ -7,3 +8,15 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)  # hash password
+
+        instance.save()
+        return instance

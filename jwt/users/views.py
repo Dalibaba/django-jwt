23987689582799cefabdff37989a0083e1663068
dlinke <david.linke@ibykus.de>
2022-09-16 09:22:43 +0200

    update registerview

--- jwt/users/views.py
+++ jwt/users/views.py
@@ -1,8 +1,14 @@
+from urllib import response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
+
 # Create your views here.
 
 
 class RegisterView(APIView):
     def post(self, request):
-        pass
-
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)

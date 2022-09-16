8db8e0ae98498af5e1b5ac184ea866a458ef48c4
dlinke <david.linke@ibykus.de>
2022-09-16 14:42:27 +0200

    add jwt to cookie

--- jwt/users/views.py
+++ jwt/users/views.py
@@ -42,4 +42,11 @@ class LoginView(APIView):
         token = jwt.encode(payload, 'secret',
                            algorithm='HS256')
 
-        return Response({"jwt": token})
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+
+        return response

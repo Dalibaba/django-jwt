8b9df2291e7d53c63e8b136ea176ff7022f0bc52
dlinke <david.linke@ibykus.de>
2022-09-19 10:48:10 +0200

    update user model and add django simple jwt

--- jwt/base/settings.py
+++ jwt/base/settings.py
@@ -123,6 +123,13 @@ STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'users.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny', ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- jwt/users/admin.py
+++ jwt/users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from users.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea, CharField
+from django import forms
+from django.db import models
 
-# Register your models here.
+
+class UserAdminConfig(UserAdmin):
+    model = User
+    list_filter = ('email',  'is_active', 'is_staff')
+    list_display = ('email', 'is_active', 'is_staff')
+
+    ordering = ('-email',)
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 20, 'cols': 60})},
+    }
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'is_active', 'is_staff')}
+         ),
+    )
+
+
+admin.site.register(User, UserAdminConfig)
